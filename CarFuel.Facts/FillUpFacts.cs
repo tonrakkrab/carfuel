80b038ee0f4672e94a498f24ec2cd34b949d689c
unknown <thanakorn.pat@THANAKORNPAT.bla.co.th>
2015-06-13 14:25:45 +0700

    Commit - Add Unit Testing Framework

---
+++ CarFuel.Facts/FillUpFacts.cs
@@ -0,0 +1,43 @@
+﻿using Carfuel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarFuel.Facts
+{
+    public class FillUpFacts
+    {
+        public class GeneralUsage
+        {
+            [Fact]
+            public void Basic()
+            {
+                // Arrange
+                var f1 = new FillUp();
+                f1.Odometer = 1000;
+                f1.IsFull = true;
+                f1.Liters = 50.0;
+                // Act
+                // Assert
+                Assert.Equal(1000, f1.Odometer);
+                Assert.Equal(true, f1.IsFull);
+                Assert.Equal(50.0, f1.Liters);
+                
+            }
+            
+            [Fact]
+            public void DefaultDateShouldBeNow()
+            {
+                // Arrange
+                var f = new FillUp();
+                // Act
+                DateTime dt = f.Date;
+                // Assert
+                Assert.Equal(expected: DateTime.Now, actual: dt);
+            }
+        }
+    }
+}
--- CarfuelModels/FillUp.cs
+++ CarfuelModels/FillUp.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Carfuel.Model
+namespace Carfuel.Models
 {
     public class FillUp
     {
+        public int Odometer { get; set; }
+
+        public bool IsFull { get; set; }
+
+        public double Liters { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public FillUp()
+        {
+            this.Date = DateTime.Now;
+        }
     }
 }
